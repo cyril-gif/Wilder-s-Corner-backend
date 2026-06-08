92b732f23ee579934c92498a56348d7393f229e5
Pascal Lantam Gbate <cyrillantam@gmail.com>
2026-06-08 04:40:28 +0000

    Add route for tracking orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,12 +4,13 @@ import {
   getMyOrders,
   getOrderById,
   updateOrderToPaid,
+  publicTrackOrder,
 } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { orderValidation } from '../validations/orderValidation.js';
 
 const router = express.Router();
-
+router.get('/track/:id', publicTrackOrder);
 router.route('/').post(protect, orderValidation, createOrder);
 router.route('/my-orders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
